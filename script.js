b050f7e758e827296fd3da0d350c9df2a598021c
Mowdat <zrida4534@gmail.com>
2024-10-04 16:08:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+// const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+// sounds.forEach(sound => {
+//     const btn = document.createElement('button')// Create button and set properties
+//     btn.classList.add('btn')// adding css on the button
 
-    btn.innerText = sound
+//     btn.innerText = sound  
 
-    btn.addEventListener('click', () => {
-        stopSongs()
+//     btn.addEventListener('click', () => {
+//         stopSongs() // stop the current sound
 
-        document.getElementById(sound).play()
-    })
+//         document.getElementById(sound).play() // Play the clicked sound
+//     })
 
-    document.getElementById('buttons').appendChild(btn)
-})
+//     document.getElementById('buttons').appendChild(btn)  // Add the button to the HTML , when its loaded
+// })
 
-function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+// function stopSongs() {s
+//     sounds.forEach(sound => {
+//         const song = document.getElementById(sound)
 
-        song.pause()
-        song.currentTime = 0;
-    })
+//         song.pause()
+//         song.currentTime = 0;
+//     })
+// }
+
+function playSound(soundId) {
+  document.getElementById(soundId).play();
 }
